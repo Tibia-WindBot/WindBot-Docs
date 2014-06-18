ec211f034bb4cb033f25bbee306ad31193462cf4
rmobis <r.mobis@gmail.com>
2014-06-18 16:30:16 -0300

    Implement Travis CI; kinda tired of having small typos and only noticing it way later

---
+++ tests.js
@@ -0,0 +1,35 @@
+require('string.prototype.endswith');
+
+var fs     = require('fs'),
+    test   = require('utest'),
+    walk   = require('walk'),
+    assert = require('assert'),
+    walker = walk.walk(__dirname, {filters: ['node_modules', '.git']});
+
+var tests = {},
+    testFunction = function(filePath) {
+    	console.log('hi1');
+    	fs.readFile(filePath, function(err, data) {
+    		if (err) throw err;
+
+    		JSON.parse(data);
+    		console.log('hi2');
+    	});
+    };
+
+walker.on('file', function(root, fileStats, next) {
+	if (fileStats.name.endsWith('.json')) {
+		var filePath = root + '/' + fileStats.name;
+		fs.readFile(filePath, function(err, data) {
+			tests[filePath] = assert.doesNotThrow.bind(null, JSON.parse.bind(null, data));
+
+			next();
+		});
+	} else {
+		next();
+	}
+});
+
+walker.on('end', function() {
+	test('validJSON', tests);
+});
